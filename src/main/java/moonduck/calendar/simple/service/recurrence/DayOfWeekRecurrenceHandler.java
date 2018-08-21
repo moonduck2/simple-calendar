ad2f2040372a9779d38c84726f435abf50836697
moonduck2 <munduk4job@gmail.com>
2018-08-21 17:49:48 +0900

    회의 시작/종료일자를 벗어난 반복일정의 경우 처리
    시작/종료 일자를 벗어난 경우는 false를 리턴

--- src/main/java/moonduck/calendar/simple/service/recurrence/DayOfWeekRecurrenceHandler.java
+++ src/main/java/moonduck/calendar/simple/service/recurrence/DayOfWeekRecurrenceHandler.java
@@ -2,14 +2,21 @@ package moonduck.calendar.simple.service.recurrence;
 
 import java.time.LocalDate;
 
+import org.springframework.stereotype.Service;
+
 import moonduck.calendar.simple.entity.Meeting;
 import moonduck.calendar.simple.entity.Recurrence;
 
+@Service
 public class DayOfWeekRecurrenceHandler implements RecurrenceHandler {
 
 	@Override
 	public boolean isOccur(LocalDate date, Meeting meeting, Recurrence recurrence) {
-		// TODO Auto-generated method stub
-		return false;
+		LocalDate startDate = meeting.getStart();
+		LocalDate endDate = meeting.getEnd();
+		if (date.isBefore(startDate) || date.isAfter(endDate)) {
+			return false;
+		}
+		return true;
 	}
 }
--- src/test/java/moonduck/calendar/simple/service/recurrence/DayOfWeekRecurrenceHandlerTest.java
+++ src/test/java/moonduck/calendar/simple/service/recurrence/DayOfWeekRecurrenceHandlerTest.java
@@ -9,6 +9,7 @@ import java.time.LocalTime;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -18,8 +19,23 @@ import moonduck.calendar.simple.enumeration.RecurrenceType;
 
 @RunWith(SpringRunner.class)
 public class DayOfWeekRecurrenceHandlerTest {
-	@Autowired
+	@InjectMocks
 	private DayOfWeekRecurrenceHandler checker;
+	
+	@Test
+	public void 시작_종료일자를_벗어난_경우_테스트() {
+		Meeting mockMeeting = mock(Meeting.class);
+		when(mockMeeting.getStart()).thenReturn(LocalDate.of(2018, 8, 1));
+		when(mockMeeting.getEnd()).thenReturn(LocalDate.of(2018, 8, 31));
+		when(mockMeeting.getStartTime()).thenReturn(LocalTime.of(10, 0));
+		when(mockMeeting.getEndTime()).thenReturn(LocalTime.of(11, 30));
+		when(mockMeeting.getMeetingRoom()).thenReturn("회의실");
+		
+		Recurrence mockRecur = mock(Recurrence.class);
+		
+		assertFalse(checker.isOccur(LocalDate.of(2018, 7, 1), mockMeeting, mockRecur));
+		assertFalse(checker.isOccur(LocalDate.of(2018, 9, 1), mockMeeting, mockRecur));
+	}
 	
 	@Test
 	public void 주1회_특정_요일_반복_일정이_있을때_테스트() {
