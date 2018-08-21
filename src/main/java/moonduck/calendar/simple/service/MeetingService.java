508860081f501ab9ac18d5169fb6643bafb60045
moonduck2 <munduk4job@gmail.com>
2018-08-21 11:55:43 +0900

    회의 추가하는 메소드 변경
    추가된 회의의 id를 리턴하도록 수정

--- src/main/java/moonduck/calendar/simple/service/MeetingService.java
+++ src/main/java/moonduck/calendar/simple/service/MeetingService.java
@@ -2,10 +2,14 @@ package moonduck.calendar.simple.service;
 
 import org.springframework.stereotype.Service;
 
+import moonduck.calendar.simple.entity.Meeting.MeetingDto;
+
 /**
  * 회의실 예약에 관한 트랜잭션 처리를 총괄한다. 
  */
 @Service
 public class MeetingService {
-
+	public int addOrUpdateMeeting(MeetingDto meeting) {
+		return 0;
+	}
 }
