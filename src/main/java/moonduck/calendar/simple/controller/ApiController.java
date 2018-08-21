384d36239feb76c57e123cacf356d5283ba21924
moonduck2 <munduk4job@gmail.com>
2018-08-21 21:23:55 +0900

    회의 전체 가져오기 컨트롤러 코드 작성
    MeetingService에서 회의실별로 필터하게 되어 있지 않아서 일단 모든 회의실의
     일정을 다 가져오도록 함

--- src/main/java/moonduck/calendar/simple/controller/ApiController.java
+++ src/main/java/moonduck/calendar/simple/controller/ApiController.java
@@ -4,6 +4,9 @@ import java.time.LocalDate;
 import java.util.Collection;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,13 +18,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import moonduck.calendar.simple.entity.Meeting;
+import moonduck.calendar.simple.service.MeetingService;
 
 @RestController
 @RequestMapping("/meeting")
 public class ApiController {
+	@Autowired
+	private MeetingService meetingService;
+	
 	@GetMapping
-	public List<Meeting> getMeetings(@RequestParam LocalDate date, Collection<String> rooms) {
-		throw new UnsupportedOperationException("not implemented");
+	public List<Meeting> getMeetings(@RequestParam @DateTimeFormat(iso = ISO.DATE) LocalDate date, 
+			@RequestParam(required = false) Collection<String> rooms) {
+		return meetingService.findMeetingByDate(date);
 	}
 	
 	@PostMapping
