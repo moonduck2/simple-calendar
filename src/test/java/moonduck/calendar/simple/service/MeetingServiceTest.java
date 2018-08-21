24029e768a222664e92f668fb27046ca39e09eb5
moonduck2 <munduk4job@gmail.com>
2018-08-21 15:17:19 +0900

    요청한 회의가 이미 존재하는 경우 에러던지도록 테스트 작성

--- src/test/java/moonduck/calendar/simple/service/MeetingServiceTest.java
+++ src/test/java/moonduck/calendar/simple/service/MeetingServiceTest.java
@@ -1,6 +1,13 @@
 package moonduck.calendar.simple.service;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,6 +20,8 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import moonduck.calendar.simple.dao.MeetingDao;
+import moonduck.calendar.simple.entity.Meeting;
+import moonduck.calendar.simple.exception.MeetingDuplicationException;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -28,8 +37,20 @@ public class MeetingServiceTest {
 		fail();
 	}
 	
-	@Test
+	@Test(expected = MeetingDuplicationException.class)
 	public void 겹칠_경우_addOrUpdate_테스트() {
-		fail();
+		List mockDuplicates = mock(List.class);
+		when(mockDuplicates.isEmpty()).thenReturn(false);
+		
+		when(mockDao.findAllPossibleDuplicate(any(String.class), any(LocalDate.class), any(LocalDate.class),
+				any(LocalTime.class), any(LocalTime.class))).thenReturn(mock(List.class));
+		Meeting mockMeeting = mock(Meeting.class);
+		when(mockMeeting.getMeetingRoom()).thenReturn("회의실");
+		when(mockMeeting.getStart()).thenReturn(LocalDate.now());
+		when(mockMeeting.getEnd()).thenReturn(LocalDate.now());
+		when(mockMeeting.getStartTime()).thenReturn(LocalTime.now());
+		when(mockMeeting.getEndTime()).thenReturn(LocalTime.now());
+		
+		service.addOrUpdateMeeting(mockMeeting);
 	}
 }
