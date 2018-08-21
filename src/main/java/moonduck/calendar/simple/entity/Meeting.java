0bcc502da7eae504cad59478ccd932d91f59cb5c
moonduck2 <munduk4job@gmail.com>
2018-08-21 11:06:34 +0900

    회의예약 Entity 정의

---
+++ src/main/java/moonduck/calendar/simple/entity/Meeting.java
@@ -0,0 +1,33 @@
+package moonduck.calendar.simple.entity;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Table(name = "meeting")
+@Entity(name = "meeting")
+public class Meeting {
+	@Column
+	private LocalDate start;
+	
+	@Column
+	private LocalDate end;
+	
+	@Column(name = "start_time")
+	private LocalTime startTime;
+	
+	@Column(name = "end_time")
+	private LocalTime endTime;
+	
+	@Column
+	private String title;
+	
+	@Column
+	private String content;
+	
+	@Column(name = "meeting_room")
+	private String meetingRoom;
+}
