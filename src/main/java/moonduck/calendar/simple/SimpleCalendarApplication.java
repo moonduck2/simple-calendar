61e05adfd4c555d9b737d264272fb37879a78a0d
moonduck2 <munduk4job@gmail.com>
2018-08-21 10:42:26 +0900

    Initial commit

---
+++ src/main/java/moonduck/calendar/simple/SimpleCalendarApplication.java
@@ -0,0 +1,12 @@
+package moonduck.calendar.simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCalendarApplication.class, args);
+	}
+}
---
+++ src/test/java/moonduck/calendar/simple/SimpleCalendarApplicationTests.java
@@ -0,0 +1,16 @@
+package moonduck.calendar.simple;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SimpleCalendarApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
