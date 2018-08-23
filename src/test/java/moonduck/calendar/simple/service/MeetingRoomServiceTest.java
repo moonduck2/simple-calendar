95abd15bf49fa850cc7106009bd14c3f4086afe5
moonduck2 <munduk4job@gmail.com>
2018-08-23 18:53:31 +0900

    회의실 생성 로직 테스트

---
+++ src/test/java/moonduck/calendar/simple/service/MeetingRoomServiceTest.java
@@ -0,0 +1,37 @@
+package moonduck.calendar.simple.service;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import moonduck.calendar.simple.dao.RoomDao;
+import moonduck.calendar.simple.dto.RoomDto;
+import moonduck.calendar.simple.entity.Room;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class MeetingRoomServiceTest {
+	@InjectMocks
+	private MeetingRoomService service;
+	
+	@Mock
+	private RoomDao roomDao;
+	
+	@Test
+	public void 새로운_회의실_만드는_메소드_테스트() {
+		RoomDto room = mock(RoomDto.class);
+		Room roomEntity = mock(Room.class);
+		when(room.toEntity()).thenReturn(roomEntity);
+		when(roomEntity.getId()).thenReturn(0);
+		when(roomDao.save(roomEntity)).thenReturn(roomEntity);
+
+		service.createOrUpdateRoom(room);
+		verify(roomDao).save(eq(roomEntity));
+	}
+}
