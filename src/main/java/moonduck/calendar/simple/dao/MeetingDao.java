fcd7f1dc9898b0f0757031e8bde1698e1028e8c0
moonduck2 <munduk4job@gmail.com>
2018-08-23 17:59:35 +0900

    불필요한 쿼리메소드를 제거하고 필요한 쿼리는 테스트 코드 작성

--- src/main/java/moonduck/calendar/simple/dao/MeetingDao.java
+++ src/main/java/moonduck/calendar/simple/dao/MeetingDao.java
@@ -1,8 +1,6 @@
 package moonduck.calendar.simple.dao;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.Collection;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
@@ -13,39 +11,16 @@ import moonduck.calendar.simple.entity.Meeting;
 
 public interface MeetingDao extends CrudRepository<Meeting, Integer> {
 	
-	@Query("select m from Meeting m where m.meetingRoom = :room"
+	/**
+	 * 회의실 ID와 기준요일/일자에 해당하는 회의 조회
+	 * @param roomId
+	 * @param baseDate
+	 * @param dayOfWeek
+	 * @return 조건에 만족하는 회의 모두 조회
+	 */
+	@Query("select m from Meeting m where m.meetingRoom.id = :room"
 			+ " and m.recurrence.dayOfWeek = :dayOfWeek"
-			+ " and (m.startDate between :startDate and :endDate or m.endDate between :startDate and :endDate)"
-			+ " and ((m.startTime > :startTime and m.startTime < :endTime) or (endTime > :startTime and endTime < :endTime))")
-	List<Meeting> findAllPossibleDuplicate(@Param("room") String room, 
-			@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate, 
-			@Param("startTime") LocalTime startTime, @Param("endTime") LocalTime endTime,
-			@Param("dayOfWeek") int dayOfWeek);
-	
-	@Query("select m from Meeting m where m.meetingRoom = :room"
-			+ " and m.recurrence.dayOfWeek = :dayOfWeek"
-			+ " and m.endDate >= :startDate")
+			+ " and m.endDate >= :baseDate")
 	List<Meeting> findAllMeetingInDate(@Param("room") int roomId, 
-			@Param("startDate") LocalDate startDate, @Param("dayOfWeek") int dayOfWeek);
-	
-	//TODO: unit test
-	@Query("select m from Meeting m where m.recurrence.dayOfWeek = :dayOfWeek and m.enabled = 1"
-			+ " and m.startDate <= :baseDate and m.endDate >= :baseDate")
-	List<Meeting> findMeetingByDate(@Param("baseDate") LocalDate baseDate, @Param("dayOfWeek") int dayOfWeek);
-
-	//TODO: unit test
-	@Query("select m from Meeting m where m.recurrence.dayOfWeek = :dayOfWeek and enabled = 1"
-			+ " and m.startDate <= :baseDate and m.endDate >= :baseDate"
-			+ " and m.meetingRoom IN :rooms")
-	List<Meeting> findMeetingByDate(@Param("baseDate") LocalDate baseDate,
-			@Param("dayOfWeek") int dayOfWeek, @Param("rooms") Collection<String> rooms);
-	
-	//TODO: unit test
-	@Query("select m from Meeting m where m.id != :id and m.meetingRoom = :room"
-			+ " and (m.startDate between :startDate and :endDate or m.endDate between :startDate and :endDate)"
-			+ " and ((m.startTime > :startTime and m.startTime < :endTime) or (endTime > :startTime and endTime < :endTime))")
-	List<Meeting> findAllPossibleDuplicateExceptId(
-			@Param("id") int id, @Param("room") String room, 
-			@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate, 
-			@Param("startTime") LocalTime startTime, @Param("endTime") LocalTime endTime);
+			@Param("baseDate") LocalDate baseDate, @Param("dayOfWeek") int dayOfWeek);
 }
--- src/test/java/moonduck/calendar/simple/dao/MeetingDaoTest.java
+++ src/test/java/moonduck/calendar/simple/dao/MeetingDaoTest.java
@@ -17,80 +17,41 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import moonduck.calendar.simple.entity.Meeting;
 import moonduck.calendar.simple.entity.Recurrence;
+import moonduck.calendar.simple.entity.Room;
 import moonduck.calendar.simple.enumeration.RecurrenceType;
 
-//TODO: 필요없는 테스트 제거(findDuplicatedPossible이라던지...)
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class MeetingDaoTest {
 	@Autowired
-	private MeetingDao dao;
-	
+	private MeetingDao meetingDao;
+
+	@Autowired
+	private RoomDao roomDao;
+
 	@Autowired
 	private RecurrenceDao recurDao;
 
-	@Before
-	public void init() {
+	@Test
+	public void 특정_기준일에_회의가_있을_경우() {
 		//07-01 ~ 08-01, 09:00 ~ 11:00, 수요일 반복
 		Recurrence recurEntity = recurDao.save(new Recurrence()
 				.setType(RecurrenceType.ONCE_A_WEEK)
 				.setDayOfWeek(DayOfWeek.WEDNESDAY.getValue()));
-				
-		dao.save(new Meeting()
-				.setMeetingRoom("회의실1")
+
+		Room room = roomDao.save(new Room().setName("회의실1"));
+		Meeting meeting = meetingDao.save(new Meeting()
+				.setMeetingRoom(room)
 				.setStartDate(LocalDate.of(2018, 7, 1))
 				.setEndDate(LocalDate.of(2018, 9, 1))
 				.setStartTime(LocalTime.of(9, 0))
 				.setEndTime(LocalTime.of(11, 0))
-				.setRecurrence(recurEntity));
-	}
+				.setRecurrence(recurEntity)
+				.setEnabled(true));
 
-	@Test
-	public void 날짜도_시간도_겹치지_않음() {
-		//01-01 ~ 05-01, 14:00 ~ 15:00
-		List<Meeting> possibleDuplicate = dao.findAllPossibleDuplicate("회의실1",
-				LocalDate.of(2018, 1, 1), LocalDate.of(2018, 5, 1), LocalTime.of(14, 0), LocalTime.of(15, 0),
-				DayOfWeek.WEDNESDAY.getValue());
+		List<Meeting> meetings = meetingDao.findAllMeetingInDate(
+				room.getId(), LocalDate.of(2018, 8, 22), DayOfWeek.WEDNESDAY.getValue());
 
-		assertEquals(0, possibleDuplicate.size());
-	}
-
-	@Test
-	public void 날짜만_겹침() {
-		//07-31 ~ 09-01, 14:00 ~ 15:00
-		List<Meeting> possibleDuplicate = dao.findAllPossibleDuplicate("회의실1",
-				LocalDate.of(2018, 7, 31), LocalDate.of(2018, 9, 1), LocalTime.of(14, 0), LocalTime.of(15, 0),
-				DayOfWeek.WEDNESDAY.getValue());
-
-		assertEquals(0, possibleDuplicate.size());
-	}
-	
-	@Test
-	public void 시간만_겹침() {
-		//01-01 ~ 05-01, 09:00 ~ 10:00
-		List<Meeting> possibleDuplicate = dao.findAllPossibleDuplicate("회의실1",
-				LocalDate.of(2018, 1, 1), LocalDate.of(2018, 5, 1), LocalTime.of(9, 0), LocalTime.of(10, 0),
-				DayOfWeek.WEDNESDAY.getValue());
-		assertEquals(0, possibleDuplicate.size());
-	}
-	
-	@Test
-	public void 날짜_시간_모두_겹침() {
-		List<Meeting> possibleDuplicate = dao.findAllPossibleDuplicate("회의실1",
-				LocalDate.of(2018, 7, 31), LocalDate.of(2018, 9, 1), LocalTime.of(9, 0), LocalTime.of(10, 0),
-				DayOfWeek.WEDNESDAY.getValue());
-		assertEquals(1, possibleDuplicate.size());
-	}
-	
-	@Test
-	public void 기준_날짜에_해당하는_회의_조회() {
-		List<Meeting> meetings = dao.findMeetingByDate(LocalDate.of(2018, 8, 1));
-		assertEquals(1, meetings.size());
-		
-		meetings = dao.findMeetingByDate(LocalDate.of(2018, 1, 1));
-		assertEquals(0, meetings.size());
-		
-		meetings = dao.findMeetingByDate(LocalDate.of(2019, 1, 1));
-		assertEquals(0, meetings.size());
+		assertEquals(Arrays.asList(meeting), meetings);
 	}
 }
