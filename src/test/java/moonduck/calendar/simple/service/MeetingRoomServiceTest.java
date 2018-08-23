48cfcd6b3e2827691dbe04a58c6749ac8c37a781
moonduck2 <munduk4job@gmail.com>
2018-08-23 19:04:13 +0900

    모든 회의실 가져오기 테스트

--- src/test/java/moonduck/calendar/simple/service/MeetingRoomServiceTest.java
+++ src/test/java/moonduck/calendar/simple/service/MeetingRoomServiceTest.java
@@ -1,7 +1,11 @@
 package moonduck.calendar.simple.service;
 
+import static org.hamcrest.CoreMatchers.hasItems;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
+
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -39,5 +43,23 @@ public class MeetingRoomServiceTest {
 	public void 회의실_삭제_메소드_테스트() {
 		service.deleteRoom(1);
 		verify(roomDao).deleteById(eq(1));
+	}
+	
+	@Test
+	public void 모든_회의실_가져오는_메소드_테스트() {
+		Room room1 = mock(Room.class);
+		RoomDto room1Dto = mock(RoomDto.class);
+		when(room1.toDto()).thenReturn(room1Dto);
+		
+		Room room2 = mock(Room.class);
+		RoomDto room2Dto = mock(RoomDto.class);
+		when(room2.toDto()).thenReturn(room2Dto);
+		
+		List<Room> mockRooms = Arrays.asList(room1, room2);
+		when(roomDao.findAll()).thenReturn(mockRooms);
+		
+		List<RoomDto> result = service.allRooms();
+		assertThat(result, hasItems(room1Dto, room2Dto));
+		assertEquals(2, result.size());
 	}
 }
