1fd51f4cc1e8c8bfdddd7af73b3848b03a99cb0d
moonduck2 <munduk4job@gmail.com>
2018-08-26 09:03:10 +0900

    회의 삭제시 반복설정도 같이 삭제

--- src/main/java/moonduck/calendar/simple/service/MeetingService.java
+++ src/main/java/moonduck/calendar/simple/service/MeetingService.java
@@ -49,10 +49,14 @@ public class MeetingService {
 	@Autowired
 	private RecurrenceService recurChecker;
 	
-	//동시에 회의실을 잡을 수 있기때문에 가장 먼저 잡은 것만 빼고 나머지는 지운다.
-	//회의 요청이 insert되었다가 delete되면 예외를 던져 회의실 선점 실패를 알린다.
+	/**
+	 * 특정 회의실에 회의를 추가한다
+	 * @param meeting 회의 스펙
+	 * @return 추가된 회의실 ID
+	 * @throws 회의가 이미 선점되었을 경우 예외 발생
+	 */
 	@Transactional
-	public int addMeeting(MeetingDto meeting) {
+	public int addMeeting(MeetingDto meeting) throws MeetingDuplicationException {
 		return addMeeting(meeting, meetingEntity ->  {
 			meetingEntity.setEnabled(true);
 			return meetingEntity.getId();
@@ -96,9 +100,10 @@ public class MeetingService {
 	 * 먼저 변경되는 시간으로 새로 만들어보고 성공할 경우 기존 것을 삭제, 실패할 경우 예외를 던진다.
 	 * @param meeting 변경될 회의 날짜/시간 정보를 담은 객체
 	 * @return 주어진 시간으로 새로 생성된 회의의 ID
+	 * @throws 변경된 시간이 이미 선점되었을 경우 예외
 	 */
 	@Transactional
-	public int modifyMeeting(MeetingDto meeting) {
+	public int modifyMeeting(MeetingDto meeting) throws MeetingDuplicationException {
 		util.normalizeMeeting(meeting);
 		Meeting oldMeetingEntity = meetingDao.findById(meeting.getId()).orElseThrow(() -> new MeetingNotFoundException(meeting.getId()));
 		
@@ -112,7 +117,7 @@ public class MeetingService {
 	}
 	
 	/**
-	 * 특정날짜의 회의를 조회한다.
+	 * 특정날짜의 회의를 조회한다. 회의가 없는 회의실도 결과에 포함된다
 	 * @param date 기준일자
 	 * @return 모든 회의실의 일정 리스트
 	 */
@@ -130,8 +135,36 @@ public class MeetingService {
 				.collect(Collectors.toList());
 	}
 	
+	/**
+	 * 회의를 삭제처리한다
+	 * @param meetingId
+	 * @throws id에 해당하는 회의가 존재하지 않을 경우 예외
+	 */
 	@Transactional
 	public void deleteMeeting(int meetingId) {
-		meetingDao.deleteById(meetingId);
+		Optional<Meeting> meetingOpt = meetingDao.findById(meetingId);
+		if (!meetingOpt.isPresent()) {
+			throw new MeetingNotFoundException(meetingId);
+		}
+		Meeting meeting = meetingOpt.get();
+		Recurrence recur = meeting.getRecurrence();
+		if (recur != null) {
+			recurDao.delete(recur);
+		}
+		meetingDao.delete(meeting);
+	}
+	
+	/**
+	 * id로 회의 정보를 가져온다
+	 * @param meetingId
+	 * @return 회의
+	 * @throws id에 해당하는 회의가 존재하지 않을 경우 예외
+	 */
+	public MeetingDto	getMeetingById(int meetingId) throws MeetingNotFoundException {
+		Optional<Meeting> meeting = meetingDao.findById(meetingId);
+		if (!meeting.isPresent()) {
+			throw new MeetingNotFoundException(meetingId);
+		}
+		return meeting.get().toDto();
 	}
 }
